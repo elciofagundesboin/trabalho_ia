5d0e55367807fb26bace6b9a53ac539343e88b5d
Elcio Fagundes Boin <elcio.fagundesboin@gmail.com>
2022-12-16 02:14:37 -0300

    Ajustados arquivos wisper e wer, incluidas musicas

--- wer.py
+++ wer.py
@@ -3,23 +3,24 @@ import jiwer
 
 # Função que calcula o wer da música
 def calcula_erro(musica):
-    kbps = 64
-    for i in range (5):
-        # ---
-        # abre o arquivo que irá gravar o WER
-        caminho_saida = 'resultado/WER-'+ musica + '-'+ str(kbps) +'.txt'
-        f_output = open(caminho_saida, "a+", encoding='utf-8')
+    # abre o arquivo que irá gravar o WER
+    caminho_saida = 'resultado/wer/'+ musica + '.txt'
+    f_output = open(caminho_saida, "a+", encoding='utf-8')
 
+    modelos = ['tiny','base','small','medium','large']
+    saidas = []
+    for modelo in modelos:
         # abre o arquivo com o texto original (letra)
         caminho_origem = 'letras/'+ musica +' - Letra.txt'
         f_origem = open(caminho_origem, 'r', encoding='utf-8')
         texto_origem = f_origem.read()
         f_origem.close()
         
-        modelos = ['tiny','base','small','medium','large']
-        for modelo in modelos:
+        kbps = 64
+        saida = []
+        for i in range (5):
             # abre o arquivo com o texto de hipotese (resultado do wisper)
-            caminho_hipotese = 'saida/'+ musica +'-'+ str(kbps) +'-'+ modelo + '.txt'
+            caminho_hipotese = 'saida_wer/'+ musica +'-'+ str(kbps) +'-'+ modelo + '.txt'
             f_hipotese = open(caminho_hipotese, 'r', encoding='utf-8')
             texto_hipotese = f_hipotese.read()
             f_hipotese.close()
@@ -30,12 +31,14 @@ def calcula_erro(musica):
             # imprime na tela e grava o resultado do WER no arquivo de saída
             print('WER ('+ caminho_origem +')('+ caminho_hipotese +'): '+ str(wer))
             print ('------------------------')
-            f_output.write(str(wer) + '\n')
-
-        # fecha o arquivo de saída
-        f_output.close()
-        # ---
-        kbps += 64
+            saida.append(wer)
+            kbps += 64
+        saidas.append(saida)
+    # grava o resultado no arquivo de saída
+    f_output.write(str(saida) + '\n')
+    # fecha o arquivo de saída
+    f_output.close()
+        
 
 # Chama as funções para realizar os cálculos
 calcula_erro('Faroeste Caboclo')
--- wisper.py
+++ wisper.py
@@ -1,35 +1,55 @@
 # Importa o modelo Wisper
 import whisper
+# Importa as bibliotecas de time
+import time
+import timeit
 
 # Função para calcular
 def processa_musica(musica):
+    # abre o arquivo que irá gravar o WER
+    caminho_saida = 'resultado/tempos/'+ musica +'.txt'
+    f_output = open(caminho_saida, "a+", encoding='utf-8')
+
     modelos = ['tiny','base','small','medium','large']
+    tempos = []
     for modelo in modelos:
         kbps = 64
+        tempo = []
         for i in range (5):
             # ---
             # Carrega o modelo
             model = whisper.load_model(modelo)
+            # Inicia o contador de tempo
+            inicio = timeit.default_timer()
             # Processa a música
             local_musica = 'audio/'+ str(kbps) +'/'+ musica +'.mp3'
             result = model.transcribe(local_musica)
+            # finaliza o contador de tempo
+            fim = timeit.default_timer()
             # Abre o arquivo de saída em modo leitura
             arquivo_saida = musica +'-'+ str(kbps) +'-'+ modelo +'.txt'
-            file = open('saida/'+ arquivo_saida, 'w+', encoding='utf-8')
+            file = open('saida_wer/'+ arquivo_saida, 'w+', encoding='utf-8')
             # Escreve no arquivo o texto gerado
             file.write(result['text'])
             # Fecha o arquivo
             file.close()
             #print(local_musica)
             #print(arquivo_saida)
+            tempo.append(fim - inicio)
             print('Processado: '+ modelo +' | '+ local_musica)
+            print('Tempo de execução: '+ str(fim - inicio) +' segundos')
             print ('------------------------')
             # ---
             kbps += 64
+        tempos.append(tempo)
+    # grava o resultado no arquivo de saída
+    f_output.write(str(tempos) + '\n')
+    # fecha o arquivo de saída
+    f_output.close()
 
 # Processa as músicas:
 # Observações:
 # 1 - informar somente o 'Nome da Musica':
 # 2 - as músicas devem estar previamente nomeadas (na pasta 'audio/[kbps]/') no padrão exemplo: "Nome da Musica.mp3"
-processa_musica('Faroeste Caboclo')
+#processa_musica('Faroeste Caboclo')
 processa_musica('Cuando Suba La Marea')
